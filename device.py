95d36b28072671a68c76031d9a928cc518cb02b7
Tian Huang <ian.h.0628@gmail.com>
2017-03-17 21:02:20 +0000

    Update device.py with new functions in i2cSnap

--- device.py
+++ device.py
@@ -2,48 +2,31 @@ import time
 
 class Device(object):
 
-	def __init__(self,interface,addr,name):
+	def __init__(self,interface,addr):
 		self.itf = interface
 		self.addr = addr
-		self.name = name
 
 	def _write(self,addrOffset,data):
-		self.itf.writeSlave(self.addr+addrOffset,data)
-
-	def _read(self,addrOffset):
-		return self.itf.readSlave(self.addr+addrOffset)
+		if isinstance(data, list):
+			self.itf.write_bytes(self.addr+addrOffset,data)
+		else:
+			self.itf.write_byte(self.addr+addrOffset,data)
+			
+	def _read(self,addrOffset,length=1):
+		if length==1:
+			return self.itf.read_byte(self.addr+addrOffset)
+		else:
+			return self.itf.read_bytes(self.addr+addrOffset,length)
 
 class GPIO(Device):
-	""" Operate a bit of a PCF8574 chip. PCF8574 is a Remote 8-Bit I/O 
+	""" Operate a PCF8574 chip. PCF8574 is a Remote 8-Bit I/O 
 	Expander for I2C BUS """
 
-	def __init__(self,interface,addr,bitOffset,name):
-		super(GPIO,self).__init__(interface,addr,name)
-		self.mask = 0x1<<bitOffset
-
 	def read(self):
-		""" Doesn't work for prototype verion of FCB FAM and PAM because all
-		workloads are LEDs """
-		result = self._read(0)
-		return result & self.mask
+		return self._read(0)
 
 	def write(self,data):
-		result = self._read(0)
-		if data:
-			"""Set one"""
-			self._write(0, result | self.mask)
-		else:
-			"""Set zero"""
-			self._write(0, result & ~self.mask)
-
-	def toggle(self):
-		result = self._read(0)
-		if result & self.mask:
-			"""Set zero"""
-			self._write(0, result & ~self.mask)
-		else:
-			"""Set one"""
-			self._write(0, result | self.mask)
+		self._write(0, data)
 
 class Temperature(Device):
 	""" Si7051 I2C Temperature Sensors """
@@ -51,9 +34,7 @@ class Temperature(Device):
 	def read(self):
 		self._write(0,0xe3)
 		time.sleep(0.01)
-		msb = self._read(0)
-		lsb = self._read(0)
-		junk = self._read(0)
+		msb,lsb,crc = self._read(0,3)
 		val = (lsb + (msb << 8)) & 0xfffc
 		temp = -46.85 + (val * 175.72) / 65536.0
 		return temp
@@ -63,10 +44,10 @@ class SerialNumber(Device):
 
 	def read(self):
 		self._write(0,0)
-		i=0
 		sn = 0x0
-		while(i<8):
-			sn = sn + self._read(0) << i
+		data = self._read(0,8)
+		for i in range(8):
+			sn = sn + data[i] << i
 		return sn
 
 
