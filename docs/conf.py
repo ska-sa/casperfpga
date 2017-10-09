9290cacad838bf16cd28af75386ecbe7cb90604e
dgorthi <deepthigorthi136@gmail.com>
2017-10-09 13:43:38 -0700

    changed theme

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -106,7 +106,6 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
-        'about.html',
         'navigation.html',
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
