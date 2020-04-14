ska-sa/casperfpga

commit 7e8555233d2c22cf7161921e33ab9f1f0d425d04

Made changes to the Logging infrastructure. Now instantiates a logging entity when creating the casperfpga object, and propagates through the classes. Updated the transport classes for now; still need to update the Memory objects (forty_gbe, etc). Exposed the method to change the log_level for the casperfpga entity. The method is common to all transports, hence it is in casperfpga.py - I implore you to take a look at the docstring. Added colouring to the log-messages printed to the terminal. {Error: Red, Warning: Orange, Info: Green, Debug: White}. Lastly, the log-file it is written to is stored in /tmp/ and follows the naming convention: casperfpga_{hostname}.log. More changes to come, including adding a custom handler for stdout and stderr. I've added these straight to devel as it does not clash with/alter major functionality.

src/transport_tapcp.py
@@ -7,7 +7,7 @@ from transport import Transport
 __author__ = 'jackh'
 __date__ = 'June 2017'
 
-LOGGER = logging.getLogger(__name__)
+# LOGGER = logging.getLogger(__name__)
 
 TFTPY = None
 
@@ -82,7 +82,10 @@ class TapcpTransport(Transport):
         Transport.__init__(self, **kwargs)
         set_log_level(logging.ERROR)
         self.t = tftpy.TftpClient(kwargs['host'], 69)
-        self._logger = LOGGER
+        try:
+            self.logger = kwargs['logger']
+        except KeyError:
+            self.logger = logging.getLogger(__name__)
         self.timeout = kwargs.get('timeout', 3)
 
     @staticmethod
@@ -95,8 +98,8 @@ class TapcpTransport(Transport):
         try:
             board = TapcpTransport(host=host_ip, timeout=0.1)
         except ImportError:
-            LOGGER.error('tftpy is not installed, do not know if %s is a Tapcp'
-                         'client or not' % str(host_ip))
+            # LOGGER.error('tftpy is not installed, do not know if %s is a Tapcp'
+            #              'client or not' % str(host_ip))
             return False
         # Temporarily turn off logging so if tftp doesn't respond
         # there's no error. Remember the existing log level so that
@@ -105,7 +108,7 @@ class TapcpTransport(Transport):
         set_log_level(logging.CRITICAL)
         if board.is_connected():
             set_log_level(log_level)
-            LOGGER.debug('%s seems to be a Tapcp host' % host_ip)
+            # LOGGER.debug('%s seems to be a Tapcp host' % host_ip)
             return True
         return False
 
@@ -191,7 +194,7 @@ class TapcpTransport(Transport):
         """
         # trigger reboot of FPGA
         self.progdev(0)
-        LOGGER.info('%s: deprogrammed okay' % self.host)
+        self.logger.info('Skarab deprogrammed okay')
 
     def write_wishbone(self, wb_address, data):
         """

src/CasperLogHandlers.py
@@ -1,0 +1,303 @@
+import logging
+import termcolors
+import datetime
+
+# from utils import get_kwarg
+
+
+def check_logging_level(logging_level):
+    """
+    Generic function to carry out a sanity check on the logging_level
+    used to setup the logger
+    :param logging_level: String input defining the logging_level:
+                             Level      | Numeric Value
+                             --------------------------
+                             CRITICAL   | 50
+                             ERROR      | 40
+                             WARNING    | 30
+                             INFO       | 20
+                             DEBUG      | 10
+                             NOTSET     | 0
+
+    :return:
+    """
+    logging_level_numeric = getattr(logging, logging_level, None)
+    if not isinstance(logging_level_numeric, int):
+        raise ValueError('Invalid Log Level: %s' % logging_level)
+    # else: Continue
+    return logging_level
+
+
+# region -- CasperStreamHandler --
+class CasperStreamHandler(logging.Handler):
+    """
+    Stream Log Handler for casperfpga messages
+    - Trying a custom logger before incorporating into corr2
+    - This inherits from the logging.Handler - Stream or File
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        New method added to test logger functionality across transport layers
+        - Need to add handlers for both Stream AND File
+        :param max_len: How many log messages to store in the FIFO
+        :return:
+        """
+
+        # logging.Handler.__init__(self)
+        super(CasperStreamHandler, self).__init__()
+
+        if len(args) > 0:
+            try:
+                kwargs['hostname'] = args[0]
+                kwargs['max_len'] = args[1]
+            except IndexError:
+                pass
+
+        self._host = kwargs['hostname']
+
+        # max_len_fifo = get_kwarg('max_len', kwargs)
+        if kwargs['max_len']:
+            self._max_len = kwargs['max_len']
+        else:
+            self._max_len = 1000
+
+        self._records = []
+
+    def emit(self, message):
+        """
+        Handle a log message
+        :param message: Log message as a string
+        :return: True/False - Success/Fail
+        """
+        if len(self._records) >= self._max_len:
+            self._records.pop(0)
+
+        self._records.append(message)
+
+        if message.exc_info:
+            print termcolors.colorize('%s: %s Exception: ' % (message.name, message.msg), message.exc_info[0:-1],
+                                      fg='red')
+        else:
+            # console_text = '{} | {}:{} - {}'.format(message.levelname, message.filename, str(message.lineno),
+            #                                        message.msg)
+            console_text = self.format(message)
+            if message.levelno == logging.DEBUG:
+                print termcolors.colorize(console_text, fg='white')
+            elif (message.levelno > logging.DEBUG) and (message.levelno < logging.WARNING):
+                print termcolors.colorize(console_text, fg='green')
+            elif (message.levelno >= logging.WARNING) and (message.levelno < logging.ERROR):
+                print termcolors.colorize(console_text, fg='yellow')
+            elif message.levelno >= logging.ERROR:
+                print termcolors.colorize(console_text, fg='red')
+            else:
+                print '%s: %s' % (message.name, message.msg)
+
+    def format(self, record):
+        """
+        :param record: Log message as a string, of type logging.LogRecord
+        :return: Formatted message
+        """
+
+        formatted_datetime = str(datetime.datetime.now())
+        # formatted_string = '{} || {} - {} - {}'.format(formatted_datetime, record.name, 'temp', 'temp')
+        # formatted_string = '{} | {} | {} - {}:{} - {}'.format(formatted_datetime, record.levelname, self._host,
+        #                                                       record.filename, str(record.lineno), record.msg)
+        formatted_string = '{} - {} | {} | {}:{} - {}'.format(record.name, formatted_datetime, record.levelname,
+                                                              record.filename, str(record.lineno), record.msg)
+
+        return formatted_string
+
+    def clear_log(self):
+        """
+        Clear the list of stored log messages
+        :return:
+        """
+        self._records = []
+
+    def set_max_len(self, max_len):
+        """
+
+        :param max_len:
+        :return:
+        """
+        self._max_len = max_len
+
+    def get_log_strings(self, num_to_print=1):
+        """
+
+        :param num_to_print:
+        :return:
+        """
+        log_list = []
+        for ctr, record in enumerate(self._records):
+            if ctr == num_to_print:
+                if record.exc_info:
+                    log_list.append('%s: %s Exception: ' % (record.name, record.msg))
+                else:
+                    log_list.append('%s: %s' % (record.name, record.msg))
+            pass
+        return log_list
+
+    def print_messages(self, num_to_print=1):
+        """
+        Print log messages stored in FIFO
+        :param num_to_print:
+        :return:
+        """
+
+        for ctr, record in enumerate(self._records):
+            if ctr == num_to_print:
+                break
+            if record.exc_info:
+                print termcolors.colorize('%s: %s Exception: ' % (record.name, record.msg), record.exc_info[0:-1],
+                                          fg='red')
+            else:
+                if record.levelno < logging.WARNING:
+                    print termcolors.colorize('%s: %s' % (record.name, record.msg), fg='green')
+                elif (record.levelno >= logging.WARNING) and (record.levelno < logging.ERROR):
+                    print termcolors.colorize('%s: %s' % (record.name, record.msg), fg='yellow')
+                elif record.levelno >= logging.ERROR:
+                    print termcolors.colorize('%s: %s' % (record.name, record.msg), fg='red')
+                else:
+                    print '%s: %s' % (record.name, record.msg)
+
+# endregion
+
+# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
+
+# region -- CasperFileHandler --
+
+
+class CasperFileHandler(logging.Handler):
+    """
+    File Log Handler for casperfpga messages
+    - Trying a custom logger before incorporating into corr2
+    - This inherits from the logging.Handler - Stream or File
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        New method added to test logger functionality across transport layers
+        - Need to add handlers for both Stream AND File
+        FileHandler needs a Filename to write to!
+        :return:
+        """
+
+        # logging.Handler.__init__(self)
+        super(CasperFileHandler, self).__init__()
+
+        if len(args) > 0:
+            try:
+                kwargs['max_len'] = args[1]
+            except IndexError:
+                pass
+
+        # max_len_fifo = get_kwarg('max_len', kwargs)
+        if kwargs['max_len']:
+            self._max_len = kwargs['max_len']
+        else:
+            self._max_len = 1000
+
+        self._records = []
+
+    def emit(self, message):
+        """
+        Handle a log message
+        :param message: Log message as a string
+        :return: True/False - Success/Fail
+        """
+        if len(self._records) >= self._max_len:
+            self._records.pop(0)
+
+        self._records.append(message)
+
+        if message.exc_info:
+            print termcolors.colorize('%s: %s Exception: ' % (message.name, message.msg), message.exc_info[0:-1],
+                                      fg='red')
+        else:
+            # console_text = '{} | {}:{} - {}'.format(message.levelname, message.filename, str(message.lineno),
+            #                                        message.msg)
+            console_text = self.format(message)
+            if message.levelno == logging.DEBUG:
+                print termcolors.colorize(console_text, fg='cyan')
+            elif (message.levelno > logging.DEBUG) and (message.levelno < logging.WARNING):
+                print termcolors.colorize(console_text, fg='green')
+            elif (message.levelno >= logging.WARNING) and (message.levelno < logging.ERROR):
+                print termcolors.colorize(console_text, fg='yellow')
+            elif message.levelno >= logging.ERROR:
+                print termcolors.colorize(console_text, fg='red')
+            else:
+                print '%s: %s' % (message.name, message.msg)
+
+    def format(self, record):
+        """
+        :param record: Log message as a string, of type logging.LogRecord
+        :return: Formatted message
+        """
+
+        formatted_datetime = str(datetime.datetime.now())
+        # formatted_string = '{} || {} - {} - {}'.format(formatted_datetime, record.name, 'temp', 'temp')
+        # formatted_string = '{} - {} | {} | {}:{} - {}'.format(self._host, formatted_datetime, record.levelname,
+        #                                                      record.filename, str(record.lineno), record.msg)
+        formatted_string = '{} - {} | {} | {}:{} - {}'.format(record.name, formatted_datetime, record.levelname,
+                                                              record.filename, str(record.lineno), record.msg)
+
+        return formatted_string
+
+    def clear_log(self):
+        """
+        Clear the list of stored log messages
+        :return:
+        """
+        self._records = []
+
+    def set_max_len(self, max_len):
+        """
+
+        :param max_len:
+        :return:
+        """
+        self._max_len = max_len
+
+    def get_log_strings(self, num_to_print=1):
+        """
+
+        :param num_to_print:
+        :return:
+        """
+        log_list = []
+        for ctr, record in enumerate(self._records):
+            if ctr == num_to_print:
+                if record.exc_info:
+                    log_list.append('%s: %s Exception: ' % (record.name, record.msg))
+                else:
+                    log_list.append('%s: %s' % (record.name, record.msg))
+            pass
+        return log_list
+
+    def print_messages(self, num_to_print=1):
+        """
+        Print log messages stored in FIFO
+        :param num_to_print:
+        :return:
+        """
+
+        for ctr, record in enumerate(self._records):
+            if ctr == num_to_print:
+                break
+            if record.exc_info:
+                print termcolors.colorize('%s: %s Exception: ' % (record.name, record.msg), record.exc_info[0:-1], fg='red')
+            else:
+                if record.levelno < logging.WARNING:
+                    print termcolors.colorize('%s: %s' % (record.name, record.msg), fg='green')
+                elif (record.levelno >= logging.WARNING) and (record.levelno < logging.ERROR):
+                    print termcolors.colorize('%s: %s' % (record.name, record.msg), fg='yellow')
+                elif record.levelno >= logging.ERROR:
+                    print termcolors.colorize('%s: %s' % (record.name, record.msg), fg='red')
+                else:
+                    print '%s: %s' % (record.name, record.msg)
+
+
+# endregion

commit 2e05e3dc30b6511acfd1ac620b0577953eca3f6d

Fix _logger -> logger
The attribute name was changed in 7e8555233d2, which
broke uses in the TapcpTransport class.

My kingdom for some unit tests.

src/transport_tapcp.py
@@ -272,56 +272,56 @@ class TapcpTransport(Transport):
         # Flash writes can take a long time, due to ~1s erase cycle
         # So set the timeout high. We'll return it to normal at the end
         old_timeout = self.timeout
-        self._logger.debug("Old timeout was %f. Setting new timeout to 1.5s" % old_timeout)
+        self.logger.debug("Old timeout was %f. Setting new timeout to 1.5s" % old_timeout)
         self.timeout = 1.5
         if(filename.endswith('.fpg')):
-            self._logger.info("Programming with an .fpg file. Checking if it is already in flash")
+            self.logger.info("Programming with an .fpg file. Checking if it is already in flash")
             header, prog, md5 = self._extract_bitstream(filename)
-            self._logger.debug("Reading meta-data from flash")
+            self.logger.debug("Reading meta-data from flash")
             meta_inflash = self.get_metadata()
             if ((meta_inflash is not None) and (meta_inflash['md5sum'] == md5) and (not force)):
-                self._logger.info("Bitstream is already on flash.")
-                self._logger.debug("Returning timeout to %f" % old_timeout)
+                self.logger.info("Bitstream is already on flash.")
+                self.logger.debug("Returning timeout to %f" % old_timeout)
                 self.timeout = old_timeout
-                self._logger.info("Booting from existing user image.")
+                self.logger.info("Booting from existing user image.")
                 self.progdev(int(meta_inflash['prog_bitstream_start']))
             else:
-                self._logger.info("Bitstream is not in flash. Writing new bitstream.")
-                self._logger.debug("Generating new header information")
+                self.logger.info("Bitstream is not in flash. Writing new bitstream.")
+                self.logger.debug("Generating new header information")
                 HEAD_LOC, PROG_LOC = self._update_metadata(filename,len(header),len(prog),md5)
                 payload = header + prog
                 complete_blocks = len(payload) // sector_size
                 trailing_bytes = len(payload) % sector_size
                 for i in range(complete_blocks):
-                    self._logger.debug("block %d of %d: writing %d bytes:" % (i+1, complete_blocks, len(payload[i*sector_size : (i+1)*sector_size])))
+                    self.logger.debug("block %d of %d: writing %d bytes:" % (i+1, complete_blocks, len(payload[i*sector_size : (i+1)*sector_size])))
                     self.blindwrite('/flash', payload[i*sector_size : (i+1)*sector_size], offset=HEAD_LOC+i*sector_size)
                     readback = self.read('/flash', len(payload[i*sector_size : (i+1)*sector_size]), offset=HEAD_LOC+i*sector_size)
                     if payload[i*sector_size : (i+1)*sector_size] != readback:
                         raise RuntimeError("Readback of flash failed!")
                 # Write the not-complete last sector (if any)
                 if trailing_bytes:
-                    self._logger.debug("writing trailing %d bytes" % trailing_bytes)
+                    self.logger.debug("writing trailing %d bytes" % trailing_bytes)
                     last_offset = complete_blocks * sector_size
                     self.blindwrite('/flash', payload[last_offset :], offset=HEAD_LOC+last_offset)
                     readback = self.read('/flash', len(payload[last_offset :]), offset=HEAD_LOC+last_offset)
                     if payload[last_offset :] != readback:
                         raise RuntimeError("Readback of flash failed!")
 
-                self._logger.debug("Returning timeout to %f" % old_timeout)
+                self.logger.debug("Returning timeout to %f" % old_timeout)
                 self.timeout = old_timeout
                 # Program from new flash image!
-                self._logger.info("Booting from new bitstream")
+                self.logger.info("Booting from new bitstream")
                 self.progdev(PROG_LOC)
 
         else:
-            self._logger.info("Programming something which isn't an .fpg file.")
-            self._logger.debug("Reading file %s" % filename)
+            self.logger.info("Programming something which isn't an .fpg file.")
+            self.logger.debug("Reading file %s" % filename)
             with open(filename,'r') as fh:
                 payload = fh.read()
             complete_blocks = len(payload) // sector_size
             trailing_bytes = len(payload) % sector_size
             for i in range(complete_blocks):
-                self._logger.debug("block %d of %d: writing %d bytes:" % (i+1, complete_blocks, len(payload[i*sector_size : (i+1)*sector_size])))
+                self.logger.debug("block %d of %d: writing %d bytes:" % (i+1, complete_blocks, len(payload[i*sector_size : (i+1)*sector_size])))
                 self.blindwrite('/flash', payload[i*sector_size : (i+1)*sector_size], offset=USER_FLASH_LOC+i*sector_size)
                 readback = self.read('/flash', len(payload[i*sector_size : (i+1)*sector_size]), offset=USER_FLASH_LOC+i*sector_size)
                 if payload[i*sector_size : (i+1)*sector_size] != readback:
@@ -329,16 +329,16 @@ class TapcpTransport(Transport):
 
             # Write the not-complete last sector (if any)
             if trailing_bytes:
-                self._logger.debug("writing trailing %d bytes" % trailing_bytes)
+                self.logger.debug("writing trailing %d bytes" % trailing_bytes)
                 last_offset = complete_blocks * sector_size
                 self.blindwrite('/flash', payload[last_offset :], offset=USER_FLASH_LOC+last_offset)
                 readback = self.read('/flash', len(payload[last_offset :]), offset=USER_FLASH_LOC+last_offset)
                 if payload[last_offset :] != readback:
                     raise RuntimeError("Readback of flash failed!")
-            self._logger.debug("Returning timeout to %f" % old_timeout)
+            self.logger.debug("Returning timeout to %f" % old_timeout)
             self.timeout = old_timeout
             # Program from new flash image!
-            self._logger.info("Booting from new bitstream")
+            self.logger.info("Booting from new bitstream")
             self.progdev(USER_FLASH_LOC)
 
     def _program_new_golden_image(self, imagefile):