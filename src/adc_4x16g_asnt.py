5d69a5ded3988ee9b5331419d3fc2149e6616dcf
Wei Liu <wei@Weis-MBP.lan>
2020-11-22 18:11:16 -0800

    update adc_4x16g_asnt.py.

--- src/adc_4x16g_asnt.py
+++ src/adc_4x16g_asnt.py
@@ -3,3 +3,9 @@ import os
 import IPython
 
 class Adc_4X16G_ASNT(object):
+    """
+    This is the class definition for the ASNT 4bit/16GSps ADC
+    """
+
+    def __init__(self, parent, device_name, device_info, initalise=False):
+        
