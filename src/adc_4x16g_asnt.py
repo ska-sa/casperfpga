4af7622d0f039f7410ebe3985b1ea45bf2d1f182
Wei Liu <wei@Weis-MBP.lan>
2020-11-23 15:41:28 -0800

    update adc_4x16g_asnt.py

--- src/adc_4x16g_asnt.py
+++ src/adc_4x16g_asnt.py
@@ -8,4 +8,21 @@ class Adc_4X16G_ASNT(object):
     """
 
     def __init__(self, parent, device_name, device_info, initalise=False):
-        
+        self.parent = parent
+        self.logger = parent.logger
+        self.name = device_name
+        self.device_info = device_info
+
+    def from_device_info(cls, parent, device_name, device_info, initialise=False, **kwargs):
+        """
+        Process device info and the memory map to get all the necessary info
+        and return a SKARAB ADC instance.
+        :param parent: The parent device, normally a casperfpga instance
+        :param device_name:
+        :param device_info:
+        :param memorymap_dict:
+        :param initialise:
+        :param kwargs:
+        :return:
+        """
+        return cls(parent, device_name, device_info, initialise, **kwargs)
