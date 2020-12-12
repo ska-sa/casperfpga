c933b278fba53600e264083be833f1e5da323a8e
liuweiseu <liuwei_seu@seu.edu.cn>
2020-12-11 23:55:20 -0800

    port xilinx spi and gpio C code to python. The code hasn't been tested.

--- src/adc_4x16g_asnt.py
+++ src/adc_4x16g_asnt.py
@@ -1,8 +1,32 @@
 import os
 import IPython
-from xspi import Xspi,Xspi_Config
+from xspi import Xspi, Xspi_Config
+from xspi_h import *
+from xgpio import XGpio, XGpio_Config
 
+#SSEL0 is DAC
+#SSEL1 is TMP125
+#SSEL2 is TMP125
+#SSEL3 is HMC988 divider
+DAC_SPI_MASK    = 0x01
+TMP0_SPI_MASK   = 0x02
+TMP1_SPI_MASK   = 0x04
+DIV_SPI_MASK    = 0x08
+#Each write to the HMC988 is 16b: {data[8:0], reg[3:0], chip[2:0]}. Chip[2:0] = 000
+#reg 4: set bit 3 = 1 (input bias) and bit 4 = 1 (bypass vreg)
+HMC988_SETUP0   = 0x1420
+#reg 2: set divide = 4
+HMC988_SETUP1   = 0x0110
 
+#The two DACs which set ADC range and offset
+VREFCRLA    = 825
+VREFCRLB    = 775
+VREFCRLC    = 700
+VREFCRLD    = 513
+VREFLSBA    = 420
+VREFLSBB    = 410
+VREFLSBC    = 410
+VREFLSBD    = 410
 
 class Adc_4X16G_ASNT(object):
     """
@@ -16,7 +40,13 @@ class Adc_4X16G_ASNT(object):
         self.block_info = device_info
         self.channel_sel = 0
         self.process_device_info(device_info)
-
+        # The following parameters are used for adc initlization
+        # They are related to xil_devices
+        self.Spi = 0
+        self.Gpio0 = 0
+        self.Gpio1 = 0
+        # in Rick's design, Gpio2 is used for capturing data, which is not needed here
+        self.Gpio3 = 0
     def from_device_info(cls, parent, device_name, device_info, initialise=False, **kwargs):
         """
         Process device info and the memory map to get all the necessary info
@@ -43,6 +73,25 @@ class Adc_4X16G_ASNT(object):
             return
         self.channel_sel = device_info['channel_sel']
     
+    def WriteHMC988(self,value):
+        self.Spi.XSpi_Abort()
+        self.Spi.XSpi_SetOptions(XSP_MASTER_OPTION | XSP_MANUAL_SSELECT_OPTION)
+        SendBuf = [0,0]
+        SendBuf[1] = value>>8
+        SendBuf[0] = value & 0xff
+        self.Spi.XSpi_SetSlaveSelect(DIV_SPI_MASK)
+        self.Spi.XSpi_Transfer(SendBuf,[],2)
+        self.Spi.XSpi_SetSlaveSelect(0xff)
+
+    def WriteDAC(self, chan, val):
+        self.Spi.XSpi_SetOptions(XSP_MASTER_OPTION | XSP_MANUAL_SSELECT_OPTION)
+        value = ((chan & 0xf)<<12) | ((val & 0x3ff)<<2)
+        SendBuf = [value & 0xff, value >>8]
+        self.Spi.XSpi_SetSlaveSelect(DAC_SPI_MASK)
+        self.Spi.XSpi_Transfer(SendBuf, [], 2)
+        #TO-DO: maybe we need some delay here
+        self.Spi.XSpi_SetSlaveSelect(0xff)
+
     def adc_init(self):
         """
             This is used for adc initlization, including:
@@ -54,6 +103,7 @@ class Adc_4X16G_ASNT(object):
 
         """
 
+        #Spi devices Init
         """
         fifo_exit               1         
         spi_slave_only          0
@@ -65,6 +115,35 @@ class Adc_4X16G_ASNT(object):
         xip_mode                0
         use_startup             0
         """
-        XCConfigPtr = Xspi_config(1,    
-                                )
-        self.XSpi_CfgInitialize()
+        ConfigPtr = Xspi_config()
+        self.Spi = Xspi(self.parent,'VCU128_axi_quad_spi')
+        self.Spi.XSpi_CfgInitialize(ConfigPtr)
+        self.Spi.XSpi_Reset()
+        self.Spi.XSpi_Start()
+        # Setup the HMC988
+        self.WriteHMC988(HMC988_SETUP0)
+        self.WriteHMC988(HMC988_SETUP1)
+        # Set all of the VREFCRLs to max (=VCC)
+        # All of the VREFLSBs to VCC-260mv
+        self.WriteDAC(1, VREFCRLA)
+        self.WriteDAC(2, VREFCRLB)
+        self.WriteDAC(3, VREFCRLC)
+        self.WriteDAC(4, VREFCRLD)
+        self.WriteDAC(5, VREFCRLA)
+        self.WriteDAC(6, VREFCRLB)
+        self.WriteDAC(7, VREFCRLC)
+        self.WriteDAC(8, VREFCRLD)
+
+        #Gpio devices Init
+        ConfigPtr0 = XGpio_Config()
+        self.Gpio0 = XGpio(self.parent, 'VCU128_adc_config')
+        self.Gpio0.XGpio_CfgInitialize(ConfigPtr0)
+        self.Gpio0.XGpio_SetDataDirection(1, 0x0)
+        ConfigPtr1 = XGpio_Config()
+        self.Gpio1 = XGpio(self.parent, 'VCU128_match_pattern_config')
+        self.Gpio1.XGpio_CfgInitialize(ConfigPtr1)
+        self.Gpio1.XGpio_SetDataDirection(1, 0x0)
+        ConfigPtr3 = XGpio_Config()
+        self.Gpio3 = XGpio(self.parent, 'VCU128_drp_config')
+        self.Gpio3.XGpio_CfgInitialize(ConfigPtr3)
+        self.Gpio3.XGpio_SetDataDirection(1, 0x0)
---
+++ src/xgpio.py
@@ -0,0 +1,97 @@
+"""
+This is used for converting xilinx axi_gpio c code to python code
+All of these code are converted from xilinx code
+**Notice: Because interrupt is diffcult to implemented in python side, interrupt code isn't converted
+"""
+
+"""
+ * Common Status Codes for All Device Drivers
+"""
+XST_SUCCESS                 = 0
+XST_FAILURE                 = 1
+XST_DEVICE_NOT_FOUND        = 2
+XST_DEVICE_BLOCK_NOT_FOUND  = 3
+XST_INVALID_VERSION         = 4
+XST_DEVICE_IS_STARTED       = 5
+XST_DEVICE_IS_STOPPED       = 6
+XST_FIFO_ERROR              = 7
+
+XIL_COMPONENT_IS_READY      = 0x11111111
+XIL_COMPONENT_IS_STARTED    = 0x22222222
+
+""" 
+ * Registers
+ * Register offsets for this device.
+""" 
+XGPIO_DATA_OFFSET   = 0x0   # Data register for 1st channel
+XGPIO_TRI_OFFSET    = 0x4   # I/O direction reg for 1st channel
+XGPIO_DATA2_OFFSET  = 0x8   # Data register for 2nd channel
+XGPIO_TRI2_OFFSET   = 0xC   # I/O direction reg for 2nd channel
+
+XGPIO_GIE_OFFSET    = 0x11C # Glogal interrupt enable register
+XGPIO_ISR_OFFSET    = 0x120 # Interrupt status register
+XGPIO_IER_OFFSET    = 0x128 # Interrupt enable register
+
+"""
+ * The following constant describes the offset of each channels data and
+ * tristate register from the base address.
+"""
+XGPIO_CHAN_OFFSET   = 8
+
+"""
+ * Interrupt Status and Enable Register bitmaps and masks
+ * Bit definitions for the interrupt status register and interrupt enable
+ * registers.
+ """
+XGPIO_IR_MASK       = 0x3 # Mask of all bits
+XGPIO_IR_CH1_MASK   = 0x1 # Mask for the 1st channel
+XGPIO_IR_CH2_MASK   = 0x2 # Mask for the 2nd channel
+
+"""
+ * Global Interrupt Enable Register bitmaps and masks
+ * Bit definitions for the Global Interrupt  Enable register
+"""
+XGPIO_GIE_GINTR_ENABLE_MASK     = 0x80000000
+
+class XGpio_Config(object):
+    def __init__(self, InterruptPresent = 0, IsDual = 0):
+        self.InterruptPresent =InterruptPresent
+        self.IsDual = IsDual
+
+class XGpio(object):
+    def __init__(self, parent, devname):
+        self.parent = parent
+        self.devname = devname
+
+        self.IsReady = 0
+        self.InterruptPresent = 0
+        self.IsDual= 0
+
+    def XGpio_WriteReg(self, RegOffset, Data):
+        self.parent.write_int(self.devname, Data,False, RegOffset/4)
+    
+    def XGpio_ReadReg(self, RegOffset):
+        return self.parent.read_int(self.devname, RegOffset/4)
+
+    def XGpio_CfgInitialize(self,Config):
+        #Set some default values.
+        self.InterruptPresent = Config.InterruptPresent
+        self.IsDual = Config.IsDual
+
+        """
+        * Indicate the instance is now ready to use, initialized without error
+        """
+        self.IsReady = XIL_COMPONENT_IS_READY
+        return (XST_SUCCESS)
+ 
+    def XGpio_SetDataDirection(self, Channel, DirectionMask):
+        self.XGpio_WriteReg(((Channel - 1) * XGPIO_CHAN_OFFSET) + XGPIO_TRI_OFFSET, DirectionMask)
+
+    def XGpio_GetDataDirection(self, Channel):
+        return self.XGpio_ReadReg(((Channel - 1) * XGPIO_CHAN_OFFSET) + XGPIO_TRI_OFFSET)
+
+    def XGpio_DiscreteRead(self, Channel):
+        return self.XGpio_ReadReg(((Channel - 1) * XGPIO_CHAN_OFFSET) + XGPIO_DATA_OFFSET)
+
+    def XGpio_DiscreteWrite(self, Channel, Data):
+        self.XGpio_WriteReg(((Channel - 1) * XGPIO_CHAN_OFFSET) + XGPIO_DATA_OFFSET, Data)
