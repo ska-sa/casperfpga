c32ec3ffa9ef0fb1940ddef71c09fd0232fcabe7
rvw <rvanwyk@ska.ac.za>
2022-05-16 12:46:30 +0200

    Added memread, memwrite and wordwrite functions to alveo transport layer
    - memread and memwrite allow for absolute addressing of axi memory
    mapped space

--- src/transport_alveo.py
+++ src/transport_alveo.py
@@ -1,5 +1,6 @@
 from transport_katcp import KatcpTransport
 import katcp
+from struct import *
 
 class AlveoFunctionError(RuntimeError):
   """Not an Alveo function"""
@@ -24,13 +25,14 @@ class AlveoTransport(KatcpTransport):
       #TODO this read needs to be standardized with the final mem map
       #reply,informs = board.blocking_request(katcp.Message.request('alveo-read','0x28000' ),timeout=timeout)
       #reply,informs = board.blocking_request(katcp.Message.request('alveo-read','0x90004' ),timeout=timeout)
-      reply,informs = board.blocking_request(katcp.Message.request('wordread','id' ),timeout=timeout)
+      reply,informs = board.blocking_request(katcp.Message.request('alveo-memread','0x28000' ),timeout=timeout)
+      #reply,informs = board.blocking_request(katcp.Message.request('wordread','id' ),timeout=timeout)
       board.stop()
 
-      #args = [(i.arguments[0], i.arguments[1]) for i in informs]
-      #if args[0][1] == '0x74736574':
+      args = [(i.arguments[0], i.arguments[1]) for i in informs]
+      if args[0][1] == '0x74736574':
       #if args[0][1] == '0xDECADE05':
-      if reply.arguments[1] == '0xdecade05':
+      #if reply.arguments[1] == '0xdecade05':       #for wordread katcp msg
         #print("Seems to be an ALVEO")   #TODO to be removed
         return True
       else:
@@ -48,10 +50,63 @@ class AlveoTransport(KatcpTransport):
 
   def reset(self):
     """
-    :return: reset the ALVEO
+    reset the ALVEO
+    :return: True/False
     """
     reply, _ = self.katcprequest(name='alveo-reset', request_timeout=self._timeout)
     return reply.arguments[0] == 'ok'
+
+
+  def memread(self, addr):
+    """
+    :param addr: absolute memory address from which to read
+    :return: string of the read value in hexadecimal
+    """
+    assert(type(addr) == int), 'Please supply numeric address (hex or dec)!'
+    addr_str='0x{:x}'.format(addr)
+
+    reply,informs = self.katcprequest(name='alveo-memread', request_timeout=self._timeout, require_ok=True,
+        request_args=(addr_str,))   #note-to-self (rvw) this comma is NB when args get unpacked
+    #print reply.arguments
+    args = [(i.arguments[0], i.arguments[1]) for i in informs]
+    #print args
+
+    return args[0][1]
+
+
+  def memwrite(self, addr, data):
+    """
+    :param addr: absolute memory address to write to
+    :param dataword: numeric data to write (four-byte word)
+    :return: string of the read value in hexadecimal
+    """
+    assert(type(addr) == int), 'Please supply numeric address (hex or dec)!'
+    assert(type(data) == int), 'Please supply numeric data (hex or dec)!'
+    assert(addr < (32*1024*1024-4)), 'Please supply address within Alveo design memory range'
+    #we know that the alveo xdma bar region0 is set to 32M NOTE this could
+    #change during dev but should be a spec once the pcie region0 size is fixed
+
+    addr_str='0x{:x}'.format(addr)
+    data_str='0x{:x}'.format(data)
+
+    reply,informs = self.katcprequest(name='alveo-memwrite', request_timeout=self._timeout, require_ok=True,
+        request_args=(addr_str,data_str,))   #note-to-self (rvw) the trailing comma is NB when args get unpacked
+    #print reply.arguments
+    args = [(i.arguments[0], i.arguments[1]) for i in informs]
+    #print args
+
+    return args[0][1] == data_str
+
+  def wordwrite(self, device_name, data, offset=0, verify=True):
+    #extend the functionality of blindwrite
+    assert(type(data) == str), 'Please supply data in string format'
+    data_int = int(data, base=16)
+    assert(data_int < pow(2,32)), 'Please supply a 32-bit-bounded data word'
+    data_packed = pack('I', data_int) 
+    super(AlveoTransport, self).blindwrite(device_name, data_packed, offset)
+    if verify == True:
+      verify_data_str = self.wordread(device_name)
+      return verify_data_str.lower() == data.lower()
 
 
   def upload_to_ram_and_program(self, filename, timeout=120):  #TODO this timeout may be too short for large images
@@ -65,8 +120,12 @@ class AlveoTransport(KatcpTransport):
     if reply.arguments[0] != 'ok':
       raise RuntimeError('%s: could not program alveo' % self.host)
 
+    if self.reset() != True:
+      raise RuntimeError('%s: could not reset alveo' % self.host)
+
+    return True
+
 
 
   def check_phy_counter(self):
     raise AlveoFunctionError("Not an Alveo function")
-  pass
